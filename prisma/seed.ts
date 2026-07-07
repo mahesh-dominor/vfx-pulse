f4a18865f5ca617c3df865d9b3feba156d9b7d59
Mahesh Chander Reddy <mcr.vfx@gmail.com>
2026-07-07 19:30:34 +0530

    fix: seed users during vercel deploy

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
 import bcrypt from "bcryptjs";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
